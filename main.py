77e7e94b9d3de724f9c3532be203ce617e6a3e9d
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2023-09-28 12:54:01 +0300

    user's online status and the time since their last visit in a simple and readable format.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import json
 import requests
-
-url = "https://sef.podkolzin.consulting/api/users/lastSeen"
-params = {'offset': 0}
+from datetime import datetime, timezone
+from dateutil.parser import parse
 
 
 class UserStatusChecker:
@@ -18,12 +17,38 @@ class UserStatusChecker:
         json_data = json.loads(response.text)
         return json_data.get('data', [])
 
+    def calculate_time_difference(self, last_seen_str):
+        last_seen_datetime = parse(last_seen_str) if last_seen_str else None
+        current_datetime = datetime.now(timezone.utc)
+        return last_seen_datetime, current_datetime
+
     def print_user_info(self, user):
+        last_seen_str = user['lastSeenDate']
+        last_seen_datetime, current_datetime = self.calculate_time_difference(last_seen_str)
+
         print("...............")
         if user['isOnline']:
             print(f"{user['nickname']} is online")
-        else:
+        elif last_seen_datetime is None:
             print(f"{user['nickname']} has no last seen date")
+        else:
+            seconds = (current_datetime - last_seen_datetime).total_seconds()
+            if 0 <= seconds <= 30:
+                print(f"{user['nickname']} seen just now")
+            elif 31 <= seconds <= 60:
+                print(f"{user['nickname']} seen less than a minute ago")
+            elif 61 <= seconds <= 3540:
+                print(f"{user['nickname']} seen a couple of minutes ago")
+            elif 3541 <= seconds <= 7140:
+                print(f"{user['nickname']} seen an hour ago")
+            elif last_seen_datetime.day == current_datetime.day and seconds > 7141:
+                print(f"{user['nickname']} seen today")
+            elif last_seen_datetime.day == current_datetime.day - 1 and seconds > 7141:
+                print(f"{user['nickname']} seen yesterday")
+            elif 1 < current_datetime.day - last_seen_datetime.day <= 7 and seconds > 7141:
+                print(f"{user['nickname']} seen this week")
+            else:
+                print(f"{user['nickname']} seen long time ago")
 
     def print_users(self):
         while self.offset < 217:
@@ -33,5 +58,6 @@ class UserStatusChecker:
             self.offset += 50
 
 
+url = "https://sef.podkolzin.consulting/api/users/lastSeen"
 checker = UserStatusChecker(url)
 checker.print_users()
